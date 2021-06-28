edda4b284a1031c85db3a0350a42938e26147103
tiennv.ppe@gmail.com <tiennv.ppe@gmail.com>
2021-06-28 15:26:21 +0700

    fix

--- app/GraphQL/Queries/DataSetQueries.php
+++ app/GraphQL/Queries/DataSetQueries.php
@@ -10,7 +10,7 @@ use App\Repositories\ResourceRepository;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\URL;
 
-class DataSetQueries1
+class DataSetQueries
 {
     private $resource_repository;
     private $api_repository;
