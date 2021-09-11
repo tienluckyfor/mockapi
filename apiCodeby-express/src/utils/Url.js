d533bffabb98b21b649b3df68673163bd3bcb37c
tiennv.ppe@gmail.com <tiennv.ppe@gmail.com>
2021-09-11 21:21:57 +0700

    debug

--- apiCodeby-express/src/utils/Url.js
+++ apiCodeby-express/src/utils/Url.js
@@ -23,6 +23,8 @@ class Url {
 
     static async isOnline(url) {
         try {
+            return true;
+
             await axios.head(url, {timeout: 1000 * 5});
             return true;
         } catch (error) {
