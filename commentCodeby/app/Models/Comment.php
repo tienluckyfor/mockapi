25ad3d9b3a9e70a6fdbf66b9ab4a68c244cd0c15
tiennv.ppe@gmail.com <tiennv.ppe@gmail.com>
2021-09-27 15:57:27 +0700

    models

--- commentCodeby/app/Models/Comment.php
+++ commentCodeby/app/Models/Comment.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
-    use HasFactory;
+
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'app_id',
+        'people_id',
+        'parent_id',
+        'unique_id',
+        'content',
+        'contents',
+    ];
+
+    protected $casts = [
+        'contents' => 'array',
+    ];
 }
--- commentCodeby/app/Models/People.php
+++ commentCodeby/app/Models/People.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class People extends Model
 {
-    use HasFactory;
+
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'app_id',
+        'unique_id',
+        'name',
+        'avatar',
+        'more',
+    ];
+
+    protected $casts = [
+        'more' => 'array',
+    ];
 }
