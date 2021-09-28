9d96fc6e0e15e21363d779b5e6941cc365c5bd51
tiennv.ppe@gmail.com <tiennv.ppe@gmail.com>
2021-09-28 07:45:22 +0700

    revert

--- commentCodeby/config/lighthouse-graphql-passport.php
+++ commentCodeby/config/lighthouse-graphql-passport.php
@@ -30,7 +30,7 @@ return [
     | the default location
     |
     */
-    'schema' => base_path('graphql/schema/auth.graphql'),
+    'schema' => base_path('graphql/auth.graphql'),
     /*
     |--------------------------------------------------------------------------
     | Username Column
