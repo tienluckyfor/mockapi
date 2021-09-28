bf6c65ff138cffc1df965901f7242f033bbd49c2
tiennv.ppe@gmail.com <tiennv.ppe@gmail.com>
2021-09-28 14:26:20 +0700

    app

--- commentCodeby/app/GraphQL/Queries/AppQueries.php
+++ commentCodeby/app/GraphQL/Queries/AppQueries.php
@@ -6,20 +6,50 @@ use App\Models\App;
 
 class AppQueries
 {
-//    private $achieve_repository;
-//
-//    public function __construct(AchieveRepository $achieve_repository)
-//    {
-//        $this->achieve_repository = $achieve_repository;
-//    }
-
     public function listApp($_, array $args)
     {
         $apps = App::select('*');
+        $perPage = $args['args']['per_page'] && is_numeric($args['args']['per_page'])
+            ? abs((int)$args['args']['per_page']) : 20;
+        $currentPage = $args['args']['current_page'] && is_numeric($args['args']['current_page']) && $args['args']['current_page'] >= -1
+            ? (int)$args['args']['current_page'] : 1;
+        $offset = $perPage == -1 ? 0 : $perPage * ($currentPage - 1);
+        $perPage += 1;
+        if (isset($args['args']['sort'])) {
+            $sort = $args['args']['sort'];
+            $apps = $apps->orderBy($sort[0], $sort[1]);
+        }
+        $total = App::selectRaw('count(*) as COUNT');
         if (!empty($args['name'])) {
             $apps = $apps->where('name', 'like', "%{$args['name']}%");
+            $total = $total->where('name', 'like', "%{$args['name']}%");
         }
-        return $apps->get();
+        $total = $total->first()->COUNT;
+        $isPrev = $currentPage == 1 ? false : true;
+        $isNext = false;
+        if ($currentPage != -1) {
+            $apps = $apps
+                ->limit($perPage)
+                ->offset($offset);
+        }
+        $apps = $apps->get();
+        if ($apps->count() == $perPage) {
+            $isNext = true;
+            $apps = $apps->slice(0, -1);
+        }
+        $perPage -= 1;
+        $pageInfo = [
+            "per_page"     => $currentPage == -1 ? $total : $perPage,
+            "current_page" => $currentPage,
+            "total_item"   => $total,
+            "total_page"   => $currentPage == -1 ? 1 : ceil($total / $perPage),
+            "is_prev"      => $isPrev,
+            "is_next"      => $isNext,
+        ];
+        return [
+            'data'     => $apps,
+            'pageInfo' => $pageInfo
+        ];
     }
 
     public function detailApp($_, array $args)
