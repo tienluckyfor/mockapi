cc395262d19d3fb4c32c0b4efe6a2adfaa310c20
tiennv.ppe@gmail.com <tiennv.ppe@gmail.com>
2021-12-21 08:48:52 +0700

    storage

--- storageCodeby/seeders/20211217003327-api.js
+++ storageCodeby/seeders/20211217003327-api.js
@@ -1,5 +1,5 @@
 'use strict';
-const {jwtDecode, } = require("helpers/jwt")
+const {jwtDecode,} = require("helpers/jwt")
 
 module.exports = {
     up: async (queryInterface, Sequelize) => {
@@ -20,7 +20,8 @@ module.exports = {
                 "region": "ap-southeast-1",
                 "accessKeyId": jwtDecode("eyJhbGciOiJIUzI1NiJ9.QUtJQVhYN0JPSEVLNzU0T0M0NUY.GtT5vMuD9SQpDdSuPj4Eyb8S7xWFVDsy39Q7tHHtzWE"),
                 "secretAccessKey": jwtDecode("eyJhbGciOiJIUzI1NiJ9.NUpUZEgwNDltUU5DeUlSbldIbUF1ZDJ0TFRuQnFVeVllNCs0OEdXaQ.K-A7QTo3MKYLweyb8KDoM8hHwgQ1u1FGD9bqLyRwd4s"),
-                "apiVersion": "2006-03-01"
+                "apiVersion": "2006-03-01",
+                "signatureVersion": "v4",
             }),
             createdAt: new Date(),
             updatedAt: new Date(),
