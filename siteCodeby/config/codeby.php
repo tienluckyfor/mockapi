482a8b53696de2e1aeec60b95cb9bacba6e90741
tiennv.ppe@gmail.com <tiennv.ppe@gmail.com>
2021-12-27 00:01:46 +0700

    siteCodeby

--- siteCodeby/config/codeby.php
+++ siteCodeby/config/codeby.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'api' => env('CODEBY_PATH_API', base_path('laravel-api/src/_class/config.php')),
-//    'api_url' => env('CODEBY_API_URL', 'https://be-mockapi.codeby.com/api/restful'),
-    'api_url' => env('CODEBY_API_URL', 'http://be.mockapi.test/api/restful'),
+    'api_url' => env('CODEBY_API_URL', 'https://be-mockapi.codeby.com/api/restful'),
+//    'api_url' => env('CODEBY_API_URL', 'http://be.mockapi.test/api/restful'),
     'path' => env('CODEBY_PATH', '/laravel-api'),
     'seo_title' => env('CODEBY_SEO_TITLE', "Example's page from Codeby API"),
     'seo_description' => env('CODEBY_SEO_DESCRIPTION', "Example's Description from Codeby API"),
