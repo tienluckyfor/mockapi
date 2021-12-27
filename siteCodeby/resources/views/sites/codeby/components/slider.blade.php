d38694973618d029e9966ceebd1763acb14b12df
tiennv.ppe@gmail.com <tiennv.ppe@gmail.com>
2021-12-27 11:11:16 +0700

    siteCodeby

--- siteCodeby/resources/views/sites/codeby/components/slider.blade.php
+++ siteCodeby/resources/views/sites/codeby/components/slider.blade.php
@@ -8,6 +8,7 @@
             return {
                 slides,
                 activeSlide: 0,
+                isPlay: true,
                 goToPrevious() {
                     this.activeSlide =
                         this.activeSlide == 0 ? this.slides.length - 1 : this.activeSlide - 1;
@@ -15,30 +16,45 @@
                 goToNext() {
                     this.activeSlide =
                         this.activeSlide == this.slides.length - 1 ? 0 : this.activeSlide + 1;
-                }
+                },
+                startPlay(){
+                    window.setInterval(() => {
+                        if(this.isPlay){
+                            this.goToNext()
+                        }
+                    }, 3000);
+                },
             };
         }
 
         const sliders = {!! json_encode($sliders) !!};
     </script>
     <div class="w-full relative">
-        <div class="mx-auto relative w-full" x-data="carouselData(sliders)">
+        <div class="mx-auto relative w-full" x-data="carouselData(sliders)"
+             x-init="startPlay()"
+             @mouseover="isPlay=false"
+             @mouseleave="isPlay=true"
+        >
             <!-- Slides -->
-            <template x-for="(slide, index) in slides" :key="index">
+            <template x-for="(slide, index) in slides" :key="index" >
                 <section x-show="activeSlide == index" x-effect="console.log(activeSlide, index)"
-                         class="relative bg-gray-800 py-32 px-6 sm:py-40 sm:px-12 lg:px-16">
-                    <div class="absolute inset-0 overflow-hidden">
+                         class="relative bg-gray-800 py-32 px-6 sm:py-40 sm:px-12 lg:px-16"
+                         style="height:550px;">
+                    <div class="absolute inset-0 overflow-hidden"
+                    >
                         <img
                                 x-bind:src="slide.image.media[0].file"
                                 alt=""
                                 class="w-full h-full object-center object-cover"/>
                     </div>
-                    <div aria-hidden="true" class="absolute inset-0 bg-gray-900 bg-opacity-50"></div>
+                    <div x-show="!slide.more.isLight" aria-hidden="true" class="absolute inset-0 bg-gray-900 bg-opacity-50"></div>
                     <div class="relative max-w-3xl mx-auto flex flex-col items-center text-center">
                         <h2 class="text-3xl font-extrabold tracking-tight text-white sm:text-4xl"
                             x-html="slide.name">-</h2>
                         <p class="mt-3 text-xl text-white" x-html="slide.content">-</p>
-                        <a href="#"
+                        <a
+                           x-show="slide.more.button.name"
+                           href="#"
                            x-bind:href="'{{$config->base_url}}'+slide.more.button.link"
                            x-html="slide.more.button.name"
                            class="mt-8 w-full block bg-white border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto">Read
