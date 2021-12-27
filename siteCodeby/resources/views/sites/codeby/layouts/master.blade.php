7d734ab9aa503f564af1873a9183407a2bca6cd4
tiennv.ppe@gmail.com <tiennv.ppe@gmail.com>
2021-12-27 16:42:34 +0700

    siteCodeby

--- siteCodeby/resources/views/sites/codeby/layouts/master.blade.php
+++ siteCodeby/resources/views/sites/codeby/layouts/master.blade.php
@@ -17,10 +17,7 @@
         <div
                 class="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
             <div>
-                @php
-                    dd($config->base_url);         
-                @endphp
-                <a href="{{$config->base_url}}" class="flex">
+                <a href="{{$config->base_url}}/" class="flex">
                     <span class="sr-only">Workflow</span>
                     <img class="h-8 w-auto sm:h-10" src="{{$media->set($con['logo'])->first()}}" alt="">
                     {{--                    <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">--}}
