5ceff0fce657b5de7a47df074faf1e4d96414587
tiennv.ppe@gmail.com <tiennv.ppe@gmail.com>
2022-01-05 16:17:11 +0700

    fix limit

--- storageCodeby/app.js
+++ storageCodeby/app.js
@@ -8,13 +8,18 @@ const postsRouter = require("routes/posts")
 const fileLocalRouter = require("routes/file_local")
 const fileS3Router = require("routes/file_s3")
 const testRouter = require("routes/test")
+var bodyParser = require('body-parser');
+
 const path = require('path')
 const cors = require('cors')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
 app.use(responseFormat)
+app.use(bodyParser.json({limit: "550mb"}));
+app.use(bodyParser.urlencoded({limit: "550mb", extended: true, parameterLimit:50000}));
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
